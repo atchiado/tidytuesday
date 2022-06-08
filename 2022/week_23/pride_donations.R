7c36e2bb571a70d60d472c115a4657bc22e51284
Anthony Chiado <anthonychiado97@gmail.com>
2022-06-08 13:43:07 -0400

    Finalized viz.

--- 2022/week_23/pride_donations.R
+++ 2022/week_23/pride_donations.R
@@ -21,14 +21,11 @@ top_companies <- static_list %>%
                    subset(Contributions >= 70000 & Company!="Grand Total")
 
 # Order data decreasing by contribution amount
-grouped_companies$Company <- factor(grouped_companies$Company,
-                                    levels = grouped_companies$Company[order(grouped_companies$Contributions)])
+top_companies$Company <- factor(top_companies$Company,
+                                levels = top_companies$Company[order(top_companies$Contributions)])
 
 
 ## Create viz ---------------
-# Highlights
-pallette <- c()
-
 # Set theme
 theme_set(theme_minimal(base_family = "Lato"))
 
@@ -39,7 +36,7 @@ theme_update(axis.title = element_blank(),
              panel.grid.minor.x = element_blank(),
              panel.grid.major.y = element_blank(),
              axis.ticks.length = unit(0, "mm"),
-             axis.line.y.left = element_line(color = "black"),
+             axis.line.y.left = element_line(color = "grey30"),
              axis.text.y = element_blank(),
              axis.text.x = element_text(family = "Lato", size = 8),
              plot.margin = margin(10, 40, 20, 40),
@@ -54,17 +51,23 @@ theme_update(axis.title = element_blank(),
              legend.position = "none")
   
 # Plot data
-ggplot(grouped_companies, aes(x = Contributions, y = Company, fill = Pride)) +
+ggplot(top_companies, aes(x = Contributions, y = Company, fill = Company)) +
   geom_col(width = 0.6) +
   scale_x_continuous(limits = c(0, 650000), breaks = seq(0, 650000, by = 100000), 
                      expand = c(0, 0), labels = scales::label_dollar(), position = "top") +
   scale_y_discrete(expand = expansion(add = c(0, 0.5))) +
-  scale_fill_manual(values = c("TRUE" = "#dc71fa", "FALSE" = "grey60")) +
-  geom_shadowtext(data = subset(grouped_companies, Contributions < 650000),
+  scale_fill_manual(values = c("Toyota" = "#f8766d", "AT&T" = "#ffae57",
+                               "Charter Communications" = "grey50",
+                               "Enterprise Products Partners" = "grey50",
+                               "Comcast" = "#feec63", "Berkshire Hathaway" = "grey50",
+                               "Valero Energy" = "grey50", "Amazon" = "#00be67",
+                               "FedEx" = "#619cff", "State Farm" = "#bf80ff")) +
+  geom_shadowtext(data = subset(top_companies, Contributions < 650000),
                   aes(Contributions, y = Company, label = Company),
                   hjust = 0, nudge_x = 4000, color = "grey40", bg.color = "grey98",
                   bg.r = 0.5, family = "Lato", size = 4) + 
   labs(title = "Rainbow Capitalism",
-       subtitle = "Graph depicts the top donators to anti-LGBTQ politicians by <span style = 'color: #dc71fa;'>**Pride-supporting**</span> and <span style = 'color: grey60;'>**non Pride-supporting**</span> companies",
+       subtitle = 'Graph depicts "Pride-supporting" and non "Pride-supporting" companies that are the top ten donators to anti-LGBTQ politicians. Some companies
+that claim to support Pride are actually financially opposed.',
        caption = "Visualization: Anthony Chiado  •  Data: Data For Progress  •  Code: atchiado/tidytuesday on GitHub  • Created for R4DS #tidytuesday")
   
