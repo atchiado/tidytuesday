066440b1591815eec95b33341ac49f5674f0fd7d
Anthony Chiado <anthonychiado97@gmail.com>
2022-06-29 15:18:12 -0400

    Finalize viz design.

--- 2022/week_26/pay_gap.R
+++ 2022/week_26/pay_gap.R
@@ -12,8 +12,8 @@ paygap <- tuesdata$paygap
 
 ## Data cleaning ---------------
 school_names <- c("Durham University", "Imperial College London", "King's College London",
-                  "THE UNIVERSITY OF MANCHESTER", "Trinity College", "University of Cambridge",
-                  "University of London", "University of Oxford", "University of York")
+                  "THE UNIVERSITY OF MANCHESTER", "University of Cambridge", "University of London",
+                  "University of Oxford", "University of St Andrews", "University of York")
 
 pay_data <- paygap %>%
   filter(employer_name %in% school_names) %>%
@@ -32,7 +32,9 @@ pay_data$quartile <- factor(pay_data$quartile, levels = c("lower_quartile", "low
                                                           "upper_middle_quartile", "top_quartile"))
 levels(pay_data$quartile) <- c("Bottom", "Lower Middle", "Upper Middle", "Top")
 
-pay_data$employer_name <- str_to_title(pay_data$employer_name)
+pay_data$employer_name <- recode(pay_data$employer_name,
+                                 "THE UNIVERSITY OF MANCHESTER" = "The University of Manchester")
+pay_data$gender <- str_to_title(pay_data$gender)
 
 
 ## Data viz ---------------
@@ -51,13 +53,17 @@ theme_update(axis.title = element_blank(),
              axis.text.x = element_text(family = "Lato", size = 9),
              axis.ticks.x = element_blank(),
              axis.ticks.y = element_blank(),
-             strip.text = element_text(family = "Lato", size = 11, hjust = 0.5),
-             strip.background = element_rect(fill = "grey85"),
-             plot.margin = margin(10, 40, 20, 40),
+             strip.text = element_text(family = "Lato", size = 11, hjust = 0.5, color = "grey30"),
+             strip.background = element_rect(fill = "grey90"),
+             legend.position = "bottom",
+             legend.title = element_blank(),
+             legend.text = element_text(family = "Lato", size = 10, face = "bold", color = "grey40"),
+             legend.margin = margin(t = 5),
+             plot.margin = margin(10, 60, 20, 40),
              plot.title = element_text(family = "Lato", color = "grey10", size = 25, face = "bold",
                                        margin = margin(t = 15)),
              plot.subtitle = element_text(family = "Lato", color = "grey30", size = 12, lineheight = 1.35,
-                                              margin = margin(t = 10, b = 20)),
+                                              margin = margin(t = 10, b = 25)),
              plot.title.position = "plot",
              plot.caption.position = "plot",
              plot.caption = element_text(family = "Lato", color = "grey30", size = 8, lineheight = 1.2, 
@@ -71,11 +77,11 @@ quartile_labs <- c("Bottom\nQuartile", "Lower\nMiddle\nQuartile",
 ggplot(pay_data, aes(x = quartile, y = proportion * 100, fill = gender)) +
   geom_col(position = "fill") +
   facet_wrap( ~ employer_name, ncol = 4) +
-  scale_fill_manual(values = c("male" = "#519481", "female" = "#e7b96e")) +
+  scale_fill_manual(values = c("Male" = "#485e92", "Female" = "#e17575")) +
   scale_x_discrete(labels = quartile_labs) + 
   scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   labs(title = "The Gender Pay Gap at Prominent British Universities",
-       subtitle = "Graph depicts the proportions of male and female university employees that make up each of four salary quartiles. In general, male employees make\nup a greater proportion of the upper quartiles, while female employees make up a greater proportion of the lower quartiles. The data source tracks\nsalaries across employers in the United Kingdom from 2018-2022",
+       subtitle = "The graph depicts the proportions of male and female university employees that make up each of four salary quartiles. In general, although most universities\nemploy more females than males, male employees make up a comparatively greater proportion of the upper quartiles, while female employees make up a\ncomparatively greater proportion of the lower quartiles. The data source tracks salaries in the United Kingdom from 2018-2022.",
        caption = "Visualization: Anthony Chiado  •  Data: UK Government's Gender Pay Gap Service  •  Code: atchiado/tidytuesday on GitHub  • Created for R4DS #tidytuesday")
   
 
