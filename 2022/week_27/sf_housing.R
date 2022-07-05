ed461b208c07b150d80e19b2522c753b8989fb83
Anthony Chiado <anthonychiado97@gmail.com>
2022-07-05 13:31:54 -0400

    Load data.

--- 2022/week_27/sf_housing.R
+++ 2022/week_27/sf_housing.R
@@ -1,9 +1,16 @@
 library(tidyverse)
 library(ggtext)
 library(showtext)
+library(sf)
 font_add_google("Lato")
 showtext_auto()
 
 
 # Load data ---------------
 tuesdata <- tidytuesdayR::tt_load(2022, week = 27)
+rent_data <- tuesdata$rent
+permit_data <- tuesdata$sf_permits
+construction_data <- tuesdata$new_construction
+
+
+# 
