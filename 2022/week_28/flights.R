8e8a636e7ac9e4e2852fd00efa94f35a0f63c63f
Anthony Chiado <anthonychiado97@gmail.com>
2022-07-13 11:31:40 -0400

    Change viz type.

--- 2022/week_28/flights.R
+++ 2022/week_28/flights.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(lubridate)
 library(ggtext)
 library(showtext)
-library(rcartocolor)
+library(RColorBrewer)
 font_add_google("Lato")
 showtext_auto()
 
@@ -13,39 +13,38 @@ flights <- tuesdata$flights
 
 
 ## Clean data --------------------
+# Identify min and max dates in the data
+max_date = max(flights$FLT_DATE)
+min_date = min(flights$FLT_DATE)
+
+# Wrangle data to be useful for viz
 flight_data <- flights %>%
-  rename(airport = APT_NAME) %>% 
-  filter(airport %in% c("iGA Istanbul Airport", "Istanbul Atatürk", "Paris-Charles-de-Gaulle", "Amsterdam - Schiphol",
-                     "Frankfurt", "Madrid - Barajas", "London - Heathrow", "Barcelona", "Paris-Orly",
-                     "Palma de Mallorca", "Munich")) %>%
-  mutate(airport = recode(airport,
-                          "iGA Istanbul Airport" = "Istanbul Airport",
-                          "Istanbul Atatürk" = "Istanbul Airport")) %>%
-  mutate(year_week = str_c(isoyear(FLT_DATE),
-                           "/",
-                           formatC(isoweek(FLT_DATE), format = "f", digits = 0, width = 2, flag = "0"))) %>%
-  mutate(week_day = wday(FLT_DATE, week_start = 1)) %>% 
-  select(airport, year_week, week_day, FLT_TOT_1) %>%
-  group_by(airport, year_week, week_day) %>%
-  summarise(flight_total = sum(FLT_TOT_1)) %>%
-  ungroup() %>% 
-  complete(airport, year_week, week_day) %>% 
-  mutate(flight_total = replace_na(flight_total, 0))
+  mutate(date_group = ifelse(FLT_DATE < ymd("2020-03-07"), "pre-covid",
+                             ifelse(FLT_DATE > ymd("2021-10-17"), "post-covid", "mid-covid"))) %>%
+  filter(APT_NAME %in% c("iGA Istanbul Airport", "Istanbul Atatürk", "Paris-Charles-de-Gaulle",
+                        "Amsterdam - Schiphol", "Frankfurt", "Madrid - Barajas", "London - Heathrow",
+                        "Athens", "Lisbon", "Rome - Fiumicino", "Vienna", "Zürich", "Kiev - Boryspil",
+                        "Oslo - Gardermoen", "Brussels", "Copenhagen - Kastrup", "Dublin",
+                        "Stockholm - Arlanda","Warszawa - Chopina")) %>%
+  mutate(APT_NAME = recode(APT_NAME,
+                         "iGA Istanbul Airport" = "Istanbul Airport",
+                         "Istanbul Atatürk" = "Istanbul Airport")) %>%
+  group_by(APT_NAME, date_group) %>%
+  summarise(flight_total = sum(FLT_TOT_1), .groups = "drop") %>%
+  rename(airport = APT_NAME) %>%
+  pivot_wider(names_from = date_group, values_from = flight_total) %>%
+  mutate(pre_covid_monthly = `pre-covid` / ((interval(min_date, ymd("2020-03-08")) %/% days(1)) / 30.4375),
+         post_covid_monthly = `post-covid` / ((interval(ymd("2021-10-18"), max_date) %/% days(1)) / 30.4375),
+         recovery_rate = post_covid_monthly / pre_covid_monthly,
+         group = as.factor(ifelse(recovery_rate < 0.75, "sub-75", ifelse(recovery_rate < 1, "sub-100", "over-100"))))
 
-# Order airport factor levels
-fact_levs <- flight_data %>% 
-  group_by(airport) %>% 
-  summarise(n = sum(flight_total)) %>% 
-  arrange(-n) %>% 
-  pull(airport)
-
-# Set aiport factor levels
-flight_data <- flight_data %>% 
-  mutate(airport = factor(airport, levels = fact_levs))
+# Order factor levels of group
+flight_data$group = factor(flight_data$group, levels = c("sub-75", "sub-100", "over-100"))
     
 
 ## Data viz --------------------
 # Set theme
+theme_set(theme_minimal(base_family = "Lato"))
 theme_update(axis.title = element_blank(),
              plot.background = element_rect(fill = "grey98", color = "grey98"),
              panel.background = element_rect(fill = "grey98", color = "grey98"),
@@ -56,14 +55,14 @@ theme_update(axis.title = element_blank(),
              axis.line.y.left = element_blank(),
              panel.spacing.x = unit(1, "cm" ),
              panel.spacing.y = unit(1, "cm" ),
-             axis.text.y = element_blank(),
-             axis.text.x = element_blank(),
              axis.ticks.x = element_blank(),
              axis.ticks.y = element_blank(),
-             strip.text.y = element_text(family = "Lato", size = 12, face = "bold", hjust = 0.5, angle = 180, color = "grey30"),
-             strip.background = element_rect(fill = "grey98"),
+             axis.text.y = element_blank(),
+             axis.text.x = element_blank(),
+             strip.text = element_text(family = "Lato", size = 8, face = "bold", hjust = 0.5, color = "grey30"),
+             strip.background = element_rect(fill = "grey98", color = "grey98"),
              legend.position = "bottom",
-             legend.background = element_rect(fill = "grey98"),
+             legend.background = element_rect(fill = "grey98", color = "grey98"),
              legend.title = element_text(size = 10, face = "bold", color = "grey30"),
              legend.text = element_text(family = "Lato", size = 8, color = "grey30"),
              legend.margin = margin(t = 5),
@@ -78,18 +77,15 @@ theme_update(axis.title = element_blank(),
                                          hjust = 0, margin = margin(t = 20)))
 
 # Plot data
-ggplot(flight_data, aes(x = year_week, y = week_day, fill = flight_total)) +
-  geom_raster() +
-  facet_wrap(~ airport, ncol = 1, strip.position = "left") +
-  scale_y_reverse(breaks = 7:1,
-                     labels = rev(c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")),
-                      sec.axis = sec_axis(~.,
-                                          breaks = 7:1,
-                                          labels = rev(c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")))) +
-  scale_x_discrete(breaks = c("2016/01", "2017/01", "2018/01", "2019/01", "2020/01", "2021/01", "2022/01"), 
-                   labels = 2016:2022) +
-  scale_fill_carto_c(name = "Number of daily flights", palette = "SunsetDark", direction = 1, limits = c(0, 1500)) +
-  guides(fill = guide_colorbar(title.position = "top")) +
+ggplot(flight_data, aes(fill = recovery_rate, ymin = 0, ymax = recovery_rate, xmax = 2, xmin = 1)) +
+  geom_rect(aes(ymin = 0, ymax = 1, xmax = 2, xmin = 1), fill = "grey98", color = "grey20", size = 0.3) +
+  geom_rect() + 
+  geom_segment(aes(y = 1, yend = 1, x = 0.5, xend = 2), color = "grey20", size = 0.6) +
+  coord_polar(theta = "y", start = 3 * pi / 2) +
+  xlim(0, 2) +
+  ylim(0, 2) +
+  facet_wrap(~ airport, labeller = labeller(airport = c()), nrow = 3) +
+  scale_fill_gradient2(low ="#f16453", mid = "#fcdc5c", high = "#beef8b", midpoint = 0.65) +
   labs(title = "Bay Area Housing Development",
        subtitle = "The graph depicts construction rates for single and multi-family housing by county in the San Francisco Bay Area. Rates indicate the\npercentage of total housing production for each county that is made up by each specific type of housing. Negative values indicate\nthat a given county reduced the amount of the specified type of housing over the evaluation period from 2008-2018.",
        caption = "Visualization: Anthony Chiado  •  Data: data.sfgov.org & Kate Pennington  •  Code: atchiado/tidytuesday on GitHub  • Created for R4DS #tidytuesday")
