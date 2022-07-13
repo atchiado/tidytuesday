e4f195d62f408e16b77c6c0770c1b7da6a4e9fd5
Anthony Chiado <anthonychiado97@gmail.com>
2022-07-13 13:24:09 -0400

    Tweak viz.

--- 2022/week_28/flights.R
+++ 2022/week_28/flights.R
@@ -28,18 +28,35 @@ flight_data <- flights %>%
                         "Stockholm - Arlanda","Warszawa - Chopina")) %>%
   mutate(APT_NAME = recode(APT_NAME,
                          "iGA Istanbul Airport" = "Istanbul Airport",
-                         "Istanbul Atatürk" = "Istanbul Airport")) %>%
+                         "Istanbul Atatürk" = "Istanbul Airport",
+                         "Paris-Charles-de-Gaulle" = "Charles De Gaule Airport",
+                         "Amsterdam - Schiphol" = "Amsterdam Airport Schiphol",
+                         "Frankfurt" = "Frankfurt am Main Airport",
+                         "Madrid - Barajas" = "Adolfo Suárez Madrid–Barajas Airport",
+                         "London - Heathrow" = "Heathrow Airport",
+                         "Athens" = "Athens International Airport",
+                         "Lisbon" = "Lisbon Airport",
+                         "Rome - Fiumicino" = "Leonardo da Vinci Fiumicino Airport",
+                         "Vienna" = "Vienna International Airport",
+                         "Zürich" = "Zürich Airport",
+                         "Kiev - Boryspil" = "Boryspil International Airport",
+                         "Oslo - Gardermoen" = "Oslo Airport Gardermoen",
+                         "Brussels" = "Brussels Airport",
+                         "Copenhagen - Kastrup" = "Copenhagen Airport",
+                         "Dublin" = "Dublin Airport",
+                         "Stockholm - Arlanda" = "Stockholm Arlanda Airport",
+                         "Warszawa - Chopina" = "Warsaw Chopin Airport")) %>%
   group_by(APT_NAME, date_group) %>%
   summarise(flight_total = sum(FLT_TOT_1), .groups = "drop") %>%
   rename(airport = APT_NAME) %>%
   pivot_wider(names_from = date_group, values_from = flight_total) %>%
   mutate(pre_covid_monthly = `pre-covid` / ((interval(min_date, ymd("2020-03-08")) %/% days(1)) / 30.4375),
          post_covid_monthly = `post-covid` / ((interval(ymd("2021-10-18"), max_date) %/% days(1)) / 30.4375),
-         recovery_rate = post_covid_monthly / pre_covid_monthly,
-         group = as.factor(ifelse(recovery_rate < 0.75, "sub-75", ifelse(recovery_rate < 1, "sub-100", "over-100"))))
+         recovery_rate = post_covid_monthly / pre_covid_monthly)
 
-# Order factor levels of group
-flight_data$group = factor(flight_data$group, levels = c("sub-75", "sub-100", "over-100"))
+# Order airports by recovery rate for viz
+flight_data$airport <- factor(flight_data$airport)
+flight_data$airport <- fct_reorder(flight_data$airport, -flight_data$recovery_rate)
     
 
 ## Data viz --------------------
@@ -53,8 +70,8 @@ theme_update(axis.title = element_blank(),
              panel.grid.major.y = element_blank(),
              panel.grid.minor.y = element_blank(),
              axis.line.y.left = element_blank(),
-             panel.spacing.x = unit(1, "cm" ),
-             panel.spacing.y = unit(1, "cm" ),
+             panel.spacing.x = unit(0.8, "cm" ),
+             panel.spacing.y = unit(0.1, "cm" ),
              axis.ticks.x = element_blank(),
              axis.ticks.y = element_blank(),
              axis.text.y = element_blank(),
@@ -63,9 +80,9 @@ theme_update(axis.title = element_blank(),
              strip.background = element_rect(fill = "grey98", color = "grey98"),
              legend.position = "bottom",
              legend.background = element_rect(fill = "grey98", color = "grey98"),
-             legend.title = element_text(size = 10, face = "bold", color = "grey30"),
+             legend.title = element_text(size = 10, hjust = 0.5, face = "bold", color = "grey30"),
              legend.text = element_text(family = "Lato", size = 8, color = "grey30"),
-             legend.margin = margin(t = 5),
+             legend.margin = margin(t = 0),
              plot.margin = margin(10, 60, 20, 40),
              plot.title = element_text(family = "Lato", color = "grey10", size = 25, face = "bold",
                                        margin = margin(t = 15)),
@@ -84,10 +101,15 @@ ggplot(flight_data, aes(fill = recovery_rate, ymin = 0, ymax = recovery_rate, xm
   coord_polar(theta = "y", start = 3 * pi / 2) +
   xlim(0, 2) +
   ylim(0, 2) +
-  facet_wrap(~ airport, labeller = labeller(airport = c()), nrow = 3) +
-  scale_fill_gradient2(low ="#f16453", mid = "#fcdc5c", high = "#beef8b", midpoint = 0.65) +
-  labs(title = "Bay Area Housing Development",
-       subtitle = "The graph depicts construction rates for single and multi-family housing by county in the San Francisco Bay Area. Rates indicate the\npercentage of total housing production for each county that is made up by each specific type of housing. Negative values indicate\nthat a given county reduced the amount of the specified type of housing over the evaluation period from 2008-2018.",
-       caption = "Visualization: Anthony Chiado  •  Data: data.sfgov.org & Kate Pennington  •  Code: atchiado/tidytuesday on GitHub  • Created for R4DS #tidytuesday")
+  facet_wrap(~ airport, labeller = labeller(airport = label_wrap_gen(width = 25)), nrow = 3) +
+  scale_fill_gradient2(low ="#e75a49", mid = "#fcdc5c", high = "#bbe576",
+                       midpoint = 0.65, labels = scales::percent) +
+  guides(fill = guide_colorbar(title = "Flight Traffic Recovery Rate",
+                               title.position = "top",
+                               barwidth = unit(75, units = "mm"),
+                               barheight = unit(3.5, unit = "mm"))) +
+  labs(title = "EU Flight Traffic Recovery",
+       subtitle = "The graph depicts the recovery rates of pre-COVID-19 commercial flight volume for the busiest airport in 18 major European\nUnion countries. A full gauge would indicate a 100% flight traffic recovery rate, meaning that since October 18th, 2021 an\nairport has averaged the same number of monthly flights as it did before March 8th, 2020.",
+       caption = "Visualization: Anthony Chiado  •  Data: Eurocontrol  •  Code: atchiado/tidytuesday on GitHub  • Created for R4DS #tidytuesday")
 
   
